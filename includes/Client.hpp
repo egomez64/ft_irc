684b5e60b4448ebb0e8f6f47cac67fd80cbdaac1
egomez <enzo.gomez64600@gmail.com>
2025-05-02 11:03:03 +0200

    t

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -7,6 +7,10 @@ class Client
 private:
 	int client_fd;
 	std::string ip_addr;
+    std::string login;
+    std::string name;
+    std::string nick_name;
+    std::string pass;
 
 public:
 	Client(){};
