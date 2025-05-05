a278db2ec273cee996fe8e6fa1dfcadf979898cf
egomez <enzo.gomez64600@gmail.com>
2025-05-05 10:12:07 +0200

    setup client

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -4,17 +4,23 @@
 
 class Client
 {
-private:
-	int client_fd;
-	std::string ip_addr;
-    std::string login;
-    std::string name;
-    std::string nick_name;
-    std::string pass;
+	private:
+		int client_fd;
+		std::string ip_addr;
+		std::string username;
+		std::string nickname;
+		std::string password;
 
-public:
-	Client(){};
-	int GetFd();
-	void SetFd(int fd);
-	void setIpAdd(std::string ipadd);
+	public:
+		Client(){};
+		int GetFd();
+		void SetFd(int fd);
+		std::string GetIpAdd();
+		void setIpAdd(std::string ipadd);
+		std::string GetUsername();
+		void SetUsername(std::string usr);
+		std::string GetNick();
+		void SetNick(std::string nick);
+		std::string GetPass();
+		void SetPass(std::string pass);
 };
--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -8,6 +8,34 @@ void Client::SetFd(int fd){
     client_fd = fd;
 }
 
+std::string Client::GetIpAdd(){
+    return this->ip_addr;
+}
+
 void Client::setIpAdd(std::string ipadd){
     ip_addr = ipadd;
+}
+
+std::string Client::GetUsername(){
+    return this->username;
+}
+
+void Client::SetUsername(std::string usr){
+    username = usr;
+}
+
+std::string Client::GetNick(){
+    return this->nickname;
+}
+
+void Client::SetNick(std::string nick){
+    nickname = nick;
+}
+
+std::string Client::GetPass(){
+    return this->password;
+}
+
+void Client::SetPass(std::string pass){
+    password = pass;
 }
